697274d2a2074498e8eb5ea4637e8c7c8241fd86
Ammy <mammy.00810@gmail.com>
2026-05-31 01:09:09 +0530

    e

--- backend/config/Mail.js
+++ backend/config/Mail.js
@@ -16,11 +16,11 @@ console.log("EMAIL:", process.env.EMAIL);
 // Create Brevo SMTP transporter
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST, // smtp-relay.brevo.com
-  port: Number(process.env.SMTP_PORT), // 587
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.SMTP_USER, // Brevo SMTP login
-    pass: process.env.SMTP_PASS, // Brevo SMTP key
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
